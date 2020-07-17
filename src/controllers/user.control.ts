828735ccf370753736e6400d8fc31f9555f5b03e
Alabi Sodiq <adisco4420@gmail.com>
2020-07-17 15:15:51 +0100

    confirm user

--- src/controllers/user.control.ts
+++ src/controllers/user.control.ts
@@ -7,7 +7,7 @@ import ModelHelper from '../helpers/model.helper';
 import EmailService from '../services/email.service';
 
 class UserController extends BaseControl {
-    public async Register(req: Request, res: Response, next: NextFunction) {
+    public async Register(req: Request, res: Response) {
         try {       
             req.body.password = hashSync(req.body.password)
             const isUnique = await ModelHelper.unique(UserModel, {key: 'email', value: req.body.email});
@@ -26,5 +26,47 @@ class UserController extends BaseControl {
             this.sendResponse({status: Status.ERROR, data: error}, res);          
         }
     } 
+
+    public async Confirm(req: Request, res: Response) { 
+        try {
+            const updateUser = await UserModel.findByIdAndUpdate((req as any).user.id, {isVerified: true}, { new: true});
+            let responseObj = {status: null, data: null};            
+            if(!updateUser) {
+                responseObj = { status: Status.UNPROCESSABLE_ENTRY, data: { msg: 'User not found'}}
+            } else {
+                const tokenData = TokenService.sign(updateUser.toJSON(), '12h');
+                responseObj = { status: Status.SUCCESS, data: { msg: 'User Confirmed', payload: tokenData}}
+            }
+            this.sendResponse(responseObj, res); 
+        } catch (error) {            
+            this.sendResponse({status: Status.ERROR, data: error}, res);   
+        }
+    }
+    // public async Login(req: Request, res: Response, next: NextFunction) {    
+    //     try {        
+    //         const user: any = await UserModel.findOne({email: req.body.email}, '+password');
+    //         let responseObj = null
+    //           if (!user) {
+    //              responseObj = new BasicResponse(Status.FAILED_VALIDATION, {msg:'Invalid Credentials'});
+    //           } else {
+    //               const isValidPassword =  compareSync(req.body.password, user.password)
+    //               if (!isValidPassword){                  
+    //                  responseObj = new BasicResponse(Status.FAILED_VALIDATION, {msg:'Invalid Credentials'});
+    //               } else {
+    //                  if(!user.isVerified) {
+    //                      responseObj = new BasicResponse(Status.UNPROCESSABLE_ENTRY, {msg:'Account is not verified'});
+    //                     } else {
+    //                         const newUser = user.toJSON();
+    //                         delete newUser['password'];
+    //                         const token = TokenService.sign(newUser, '12h')   
+    //                         responseObj = new BasicResponse(Status.SUCCESS, {msg:'User Login', data: token});
+    //                  }
+    //             }
+    //         }          
+    //         this.sendResponse(responseObj, req, res);
+    //     } catch (error) {
+    //         this.sendResponse(new BasicResponse(Status.ERROR, error), req, res);           
+    //     }
+    // }
 }
 export default new UserController
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,17 +2,23 @@ import { NextFunction, Request, Response, Router } from "express";
 import UserController from '../controllers/user.control';
 import Joi from '../middlewares/validator.midware';
 import UserVtor from '../validations/user.validator'
+import AuthMidWare from '../middlewares/auth.midware';
 
 class UserRoute  {
     constructor() {}
-
     public loadRoutes(prefix: String, router: Router) {
         this.initRegister(prefix, router);
+        this.initConfirm(prefix, router);
     }
     private initRegister(prefix: String, router: Router): any { 
-        router.post(prefix + "/register", Joi.vdtor(UserVtor.Register), (req: Request, res: Response, next: NextFunction) => {
-        UserController.Register(req, res, next)
+        router.post(prefix + "/register", Joi.vdtor(UserVtor.Register), (req: Request, res: Response) => {
+        UserController.Register(req, res)
         })
     }
+    private initConfirm(prefix: String, router: Router): any { 
+        router.get(prefix + "/confirm-email", AuthMidWare, (req, res: Response) => {
+          UserController.Confirm(req, res)
+        })
+      }
 }
 export default new UserRoute;
